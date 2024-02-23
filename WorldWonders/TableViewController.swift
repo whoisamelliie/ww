9b4100305e346e1afe85baf547272bbf1f3d1555
Amelie Baimukanova <ameliebaimukanova@MacBook-Air-Amelie.local>
2024-02-23 15:58:03 +0600

    first commit

--- WorldWonders/TableViewController.swift
+++ WorldWonders/TableViewController.swift
@@ -18,20 +18,12 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-        
-//        let wonder = WorldWonder(name: "test", region: "test", location: "test", flag: "test", picture: "test")
-//        arrayWonders.append(wonder)
-//        tableView.reloadData()
         
         
         refreshControl = UIRefreshControl()
         refreshControl?.addTarget(self, action: #selector(handleRefresh), for: .valueChanged)
         tableView.addSubview(refreshControl!)
+        loadData()
     }
     @objc func handleRefresh() {
         if isLoading{
